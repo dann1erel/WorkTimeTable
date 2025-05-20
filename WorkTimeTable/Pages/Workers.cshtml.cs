da198ad08a67afcbf8418e61d7f4f317a5f3e402
dann1erel <dannierel0@gmail.com>
2025-05-20 17:43:27 +0300

    добавлена страница "работник" с функционалом добавить удалить изменить, но пока только для имени и должности

--- WorkTimeTable/Pages/Workers.cshtml.cs
+++ WorkTimeTable/Pages/Workers.cshtml.cs
@@ -1,12 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WorkTimeTable.DataBase;
 
 namespace WorkTimeTable.Pages
 {
-    public class WorkersModel : PageModel
+    [IgnoreAntiforgeryToken]
+    public class WorkersModel(ApplicationContext db) : PageModel
     {
-        public void OnGet()
+        // для получения данных из бд
+        public List<Worker> Workers { get; private set; } = [];
+        // для отправки данных в бд
+        [BindProperty]
+        public Worker Worker { get; set; } = new();
+
+        public async Task<IActionResult> OnGet()
         {
+            Workers = await db.Worker.AsNoTracking().ToListAsync();
+            return Page();
         }
+        public async Task<IActionResult> OnPostAddAsync()
+        {
+            db.Worker.Add(Worker);
+            await db.SaveChangesAsync();
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostRemoveAsync(int id)
+        {
+            var worker = await db.Worker.FindAsync(id);
+            if (worker != null)
+            {
+                db.Worker.Remove(worker);
+                await db.SaveChangesAsync();
+            }
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostEditAsync(int id, string name, string position) // добавить department
+        {
+            var worker = await db.Worker.FindAsync(id);
+            if (worker != null)
+            {
+                worker.Name = name;
+                worker.Position = position;
+                db.Worker.Update(worker);
+                await db.SaveChangesAsync();
+            }
+            return RedirectToPage();
+        }
+
     }
 }
